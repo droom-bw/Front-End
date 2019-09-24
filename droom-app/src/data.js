25ae96c69ce193525b9718a56e1270b52f3dc3a8
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-24 15:45:35 -0400

    create data file

---
+++ droom-app/src/data.js
@@ -0,0 +1,80 @@
+export const companies = [
+  {
+    id: 0,
+    name: "Lambda School",
+    email: "lambda@email.com",
+    description:
+      "Lambda School is an immersive bootcamp offering a variety of courses and such",
+    job: {
+      company: "Lambda School",
+      title: "Developer",
+      description:
+        "Lambda School needs a fullstack developer to perform alchemy",
+      salary: 1000000
+    },
+    matches: []
+  },
+  {
+    id: 1,
+    name: "Apple",
+    email: "apple@email.com",
+    description:
+      "Apple is an immersive bootcamp offering a variety of courses and such",
+    job: {
+      company: "Apple",
+      title: "Developer",
+      description: "Apple needs a fullstack developer to perform alchemy",
+      salary: 1000000
+    },
+    matches: []
+  },
+  {
+    id: 2,
+    name: "Yahoo",
+    email: "yahoo@email.com",
+    description:
+      "Yahoo is an immersive bootcamp offering a variety of courses and such",
+    job: {
+      company: "Yahoo",
+      title: "Developer",
+      description: "Yahoo needs a fullstack developer to perform alchemy",
+      salary: 1000000
+    },
+    matches: []
+  }
+]
+
+export const seekers = [
+  {
+    id: 0,
+    name: "Matt Gill",
+    email: "mattgill@email.com",
+    location: "Baltimore, MD",
+    resume: "",
+    matches: []
+  },
+  {
+    id: 0,
+    name: "Devin Bielejec",
+    email: "devinb@email.com",
+    location: "Syracuse, NY",
+    resume: "",
+    matches: []
+  },
+  {
+    id: 0,
+    name: "Ian Schwartz",
+    email: "ians@email.com",
+    location: "Pasadena, CA",
+    resume: "",
+    matches: []
+  },
+  {
+    id: 0,
+    name: "Idir Abderahim",
+    email: "idira@email.com",
+    location: "Paris, France",
+    resume: "",
+    matches: []
+  }
+]
