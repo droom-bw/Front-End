fd5f11d4610e351f64338c7b08362f5dd0de6908
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-24 15:45:46 -0400

    remove unnecessary render

--- droom-app/src/index.js
+++ droom-app/src/index.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from "react-router-dom";
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import * as serviceWorker from "./serviceWorker"
+import { BrowserRouter as Router } from "react-router-dom"
 import { createStore, applyMiddleware } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
 import { Provider } from "react-redux"
 import thunk from "redux-thunk"
 import { rootReducer } from "./store/reducers"
-
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
 const store = createStore(
   rootReducer,
