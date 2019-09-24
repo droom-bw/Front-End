a68fa3373c1d6ba4960d147a40a3c7bc9876f240
mxxt1 <matt@dustcloudlabs.com>
2019-09-24 12:59:16 -0400

    CompanyProfile.js structure, cleaned up git issues

---
+++ droom-app/src/components/Profile/CompanyProfile.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from "react";
+import CompanyCard from '../Home/CompanyCard';
+import axios from 'axios';
+
+const CompanyProfile = (props) => {
+
+const [companyID, setCompanyID] = useState("");
+
+useEffect(() => {
+
+
+
+
+
+
+})
+
+
+
+return (
+
+
+
+    
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
