eba6a421962ca6f8704876b621f3c74b4b13effc
mxxt1 <matt@dustcloudlabs.com>
2019-09-24 13:43:15 -0400

    structure for get request in CompanyProfile.js

--- droom-app/src/components/Profile/CompanyProfile.js
+++ droom-app/src/components/Profile/CompanyProfile.js
@@ -3,25 +3,32 @@ import CompanyCard from '../Home/CompanyCard';
 import axios from 'axios';
 
 const CompanyProfile = (props) => {
-
+//const [companyList, setCompanyList] = useState([]);
 const [companyID, setCompanyID] = useState("");
 
 useEffect(() => {
-
-
-
-
-
-
-})
+    axios.get(//API HERE)
+    .then(res => {
+        const data = {res.data}
+        console.log(data);
+        setCompanyID(data);
+    })
+    .catch(error => {
+        console.log(`Error getting data: `, error);
+    });
+},[]);
 
 
 
 return (
+<React.Fragment>
+{companyList}
+
+
+</React.Fragment>
 
 
 
-    
 );
 
 
