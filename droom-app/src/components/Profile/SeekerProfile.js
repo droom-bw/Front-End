0b2efcc4ab31790e43afd21ce8889da0e5e62ba8
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-24 15:48:00 -0400

    no changes

--- droom-app/src/components/Profile/SeekerProfile.js
+++ droom-app/src/components/Profile/SeekerProfile.js
@@ -1,29 +1,28 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import SeekerCard from "../Home/SeekerCard";
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import SeekerCard from "../Home/SeekerCard"
 
 export default function SeekerProfile() {
-    const [seeker, setSeeker] = useState([]);
+  const [seeker, setSeeker] = useState([])
 
-    useEffect(() => {
-        axios
-        .get('https://rickandmortyapi.com/api/character/')
-        .then(res => {
-          const seekerData = res.data.results;
-          console.log("this is the data", seekerData)
-          setSeeker(seekerData);
-        })
-        .catch(err => {
-          console.log("This is an error", err);
-        });
-    
-      }, []);
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(res => {
+        const seekerData = res.data.results
+        console.log("this is the data", seekerData)
+        setSeeker(seekerData)
+      })
+      .catch(err => {
+        console.log("This is an error", err)
+      })
+  }, [])
 
-      return (
-          <div>
-              {seeker.map(item => {
-                  return <SeekerCard key={item.name} item={item} />;
-              })}
-          </div>
-      );
+  return (
+    <div>
+      {seeker.map(item => {
+        return <SeekerCard key={item.name} item={item} />
+      })}
+    </div>
+  )
 }
