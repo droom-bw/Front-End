7609054810b8b2d507360b25641408b0cec78819
AbdelIdir <homeworkoracle@protonmail.com>
2019-09-25 15:59:38 +0200

    turned the nav bar into a drop down menu.Changed the design, added the image logo for Droom back.Spread out. Just need to adjust heights now.

--- droom-app/src/App.js
+++ droom-app/src/App.js
@@ -9,7 +9,7 @@ import { Route } from "react-router-dom"
 import HomePage from "./components/Home/HomePage"
 import LoginComponent from "./components/Login"
 import Profile from "./components/Profile/Profile"
-
+import MatchList from "./components/Home/MatchesList"
 
 function App() {
   return (
@@ -22,7 +22,7 @@ function App() {
 
       <Route exact path="/" component={LoginComponent} />
       <Route exact path="/login" component={LoginComponent} />
-
+      <Route exact path="/matches" component={MatchList} />
       <Route exact path="/home" component={HomePage} />
       <Route exact path="/profile" component={Profile} />
     </div>
--- droom-app/src/components/Navbar.js
+++ droom-app/src/components/Navbar.js
@@ -1,31 +1,45 @@
-import React from "react"
+import React from "react";
 import {
   Tag,
   TagIcon,
   Icon,
   TagLabel,
   Stack,
-  TagCloseButton
-} from "@chakra-ui/core"
+  TagCloseButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuGroup,
+  MenuDivider,
+  MenuOptionGroup,
+  MenuItemOption,
+  Button
+} from "@chakra-ui/core";
 
-import { Link as RouterLink } from "react-router-dom"
-import { Link } from "@chakra-ui/core"
-import styled from "styled-components"
-import { Image } from "@chakra-ui/core"
+import { Link as RouterLink } from "react-router-dom";
+import { Link } from "@chakra-ui/core";
+import styled from "styled-components";
+import { Image } from "@chakra-ui/core";
 
 export default function NavBar() {
   const ContainNav = styled.div`
     display: flex;
     flex-direction: row;
-    justify-content: flex-end;
-    margin-right: 50px;
-    height: 30px;
-    justify-content: baseline;
+    justify-content: space-around;
+    margin: 0 95px 0 95px;
+    /* align-items: flex-end; */
+    
+    
+    
+  
+  `;
+  const ImgCont = styled.div`
   `
 
   return (
     <ContainNav>
-      <Image
+      {/* <Image
         size="100px"
         objectFit="cover"
         src="https://trello-attachments.s3.amazonaws.com/5d84f15f96af3e57f163ac23/5d8912e25b9b4611ab2e7d9f/96d61df1c18110078a6578764af62d3c/DroomLogo.png"
@@ -43,7 +57,7 @@ export default function NavBar() {
         </Tag>
 
         {/* You can also use custom svg icons */}
-        <Tag variantColor="teal">
+      {/* <Tag variantColor="teal">
           <TagLabel>
             <Link as={RouterLink} to="/login">
               Log In
@@ -67,9 +81,49 @@ export default function NavBar() {
           </TagLabel>
           <TagIcon icon="triangle-up" />
         </Tag>
-      </Stack>
+      </Stack> */}
+<Image
+        size="100px"
+        objectFit="cover"
+        src="https://trello-attachments.s3.amazonaws.com/5d84f15f96af3e57f163ac23/5d8912e25b9b4611ab2e7d9f/96d61df1c18110078a6578764af62d3c/DroomLogo.png"
+        alt="Segun Adebayo"
+      />
+      <Menu>
+        
+        <MenuButton as={Button} rightIcon="chevron-down">
+          <Icon name="settings" />
+          &ensp;Menu
+        </MenuButton>
+
+        <MenuList>
+          <Link as={RouterLink} to="/home">
+            <MenuItem>
+              <Icon name="check" />
+              &ensp;Home
+            </MenuItem>
+          </Link>
+          <Link as={RouterLink} to="/profile">
+            <MenuItem>
+              <Icon name="view" />
+              &ensp;Profile
+            </MenuItem>
+          </Link>
+          <Link as={RouterLink} to="/matches">
+            <MenuItem>
+              <Icon name="bell" />
+              &ensp;Matches
+            </MenuItem>
+          </Link>
+          <Link as={RouterLink} to="/login">
+            <MenuItem>
+              <Icon name="triangle-down" />
+              &ensp;Log In
+            </MenuItem>
+          </Link>
+        </MenuList>
+      </Menu>
     </ContainNav>
-  )
+  );
 }
 
 // import {Link as RouterLink} from "react-router-dom"
